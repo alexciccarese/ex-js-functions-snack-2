3cebcfd361a9b7e6c94fb3a746cbcac912b5600f
alexciccarese <alex.ciccarese@gmail.com>
2025-06-03 13:38:24 +0200

    Add HTML structure and implement quadrato function in JavaScript

---
+++ 02/script.js
@@ -0,0 +1,6 @@
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+function quadrato(a) {
+  return a * a
+}
+
+console.log(quadrato(5));
