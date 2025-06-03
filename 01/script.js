bcf4189aa587e3a52838f2508974d8dbabbdb3ab
alexciccarese <alex.ciccarese@gmail.com>
2025-06-03 12:54:24 +0200

    first commit

---
+++ 01/script.js
@@ -0,0 +1,7 @@
+
+/* 
+- Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+- Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+- Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. 
+*/
+
