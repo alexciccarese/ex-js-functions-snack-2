2a71a0f3f90ea268ce2664d5102ec389962d394d
alexciccarese <alex.ciccarese@gmail.com>
2025-06-03 13:03:42 +0200

    Refactor somma function definitions and add comments for clarity

--- 01/script.js
+++ 01/script.js
@@ -5,8 +5,23 @@
 - Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. 
 */
 
+// - Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
 function somma(a, b) {
   return a + b
 }
 
-console.log(somma(5,6));
+console.log(somma(5,6)); //11
+
+
+// - Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+const somma = function(a, b) {
+  return a + b
+}
+
+console.log(somma(2,4)); //6
+
+
+// - Quindi, riscrivi la funzione somma con la sintassi delle arrow functions. 
+const somma = (a, b) => {return a + b}
+
+console.log(somma(5,9)); //14
